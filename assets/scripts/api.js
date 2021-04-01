529a3d94ab466aeaf09da86ddaec4d9d2c2164af
Hunter Barton <axiysfire@gmail.com>
2021-04-01 01:16:54 -0500

    added where to watch functionality

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -3,6 +3,10 @@ const MOVIE_DB_API = 'dc8aba4647cddc4a95994e5b2b372138'
 const MOVIE_DB_ENDPOINT = 'https://api.themoviedb.org';
 const MOVIE_DB_IMAGE_ENDPOINT = 'https://image.tmdb.org/t/p/w500';
 const DEFAULT_POST_IMAGE = 'https://via.placeholder.com/150';
+const WTW_URL = "https://watch-here.p.rapidapi.com/wheretowatch";
+const RAPID_KEY = "0105f13aa9msh4ce35e271a592d6p1f8582jsn7e0ff44b7258";
+const RAPID_HOST = "watch-here.p.rapidapi.com";
+
     
 function requestMovies(url, onComplete, onError) {
     fetch(url)
@@ -42,5 +46,38 @@ function searchPopularMovie() {
 
 function searchMovie(value) {
     const url = generateMovieDBUrl('/search/movie') + '&query=' + value;
-    requestMovies(url, renderSearchMovies, handleGeneralError);
+    const renderSearch = renderSearchMovies.bind({ title: value});
+    requestMovies(url, renderSearch, handleGeneralError);
 }
+function getVideosByMovieId(movieId, content) {
+    const url = generateMovieDBUrl(`/movie/${movieId}/videos`);
+    const render = getMovieFromId();
+    requestMovies(url, render, handleGeneralError);
+} 
+async function getMovieFromId(movieId){
+    const url = generateMovieDBUrl(`/movie/${movieId}`);      
+    const res = await fetch(url);
+    if(!res.ok) handleGeneralError;
+    const data = await res.json();
+    return await data;        
+}
+
+function getWhereToWatch(title, type, imgUrl){       
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": WTW_URL,
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
+            "x-rapidapi-key": RAPID_KEY,
+            "x-rapidapi-host": RAPID_HOST
+        },
+        "processData": false,
+        "data": `{\r\n    \"mediaType\": \"${type}\",\r\n \"platform\":true,\r\n    \"title\": \"${title}\"\r\n}`
+    };
+    $.ajax(settings).done(function (response) {
+       displayWhereToWatch(JSON.parse(response), imgUrl);      
+    });
+ }
+ 
